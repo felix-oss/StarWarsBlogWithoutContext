0c0f1dde319a312e11f84b61e128e21a50628d00
felix-oss <uv.mhjl@gmail.com>
2020-12-22 22:33:22 +0000

    update

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,9 +2,11 @@ import React from "react";
 import "../../styles/home.scss";
 
 export const Home = () => (
-	<div className="card d-flex justify-content-center" style={{ width: "60%", height: "20%" }}>
-		<div className="card-body d-flex justify-content-center">
-			<h5 className="card-title">Card title</h5>
+	<div className="d-flex justify-content-center mainDiv1">
+		<div className="card" style={{ width: "60%", height: "20%" }}>
+			<div className="card-body d-flex justify-content-center">
+				<h5 className="card-title">Card title</h5>
+			</div>
 		</div>
 	</div>
 );
